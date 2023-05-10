b164fa541e0af94783cb6298ac01cbb1c56201b6
flavio herrera <herreraflavio0@gmail.com>
2023-05-10 09:17:32 -0700

    updated port

--- server.js
+++ server.js
@@ -2,14 +2,11 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
-// app.get("/", (req, res) => {
-//   res.send("Hello World! This is Flavio Herrera");
-// });
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 443;
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
