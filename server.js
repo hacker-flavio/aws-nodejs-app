ff49e3401711982aafe2cb03ccaba5daf6f43543
flavio herrera <herreraflavio0@gmail.com>
2023-06-07 11:19:48 -0700

    addded react front end

--- server.js
+++ server.js
@@ -2,8 +2,15 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
+// app.get("/", (req, res) => {
+//   res.sendFile(path.join(__dirname, "index.html"));
+// });
+
+// Static files
+app.use(express.static(path.resolve(__dirname, "./frontend/build")));
+
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "index.html"));
+  res.sendFile(path.join(__dirname, "./frontend/build", "index.html"));
 });
 
 const port = process.env.PORT || 443;
